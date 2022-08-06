a44590470ca198d5f6666e550b70e8d980a9fb00
Haidros <rosariohanspatrickadrian@gmail.com>
2022-08-06 23:37:23 +0800

    Person and employee

---
+++ employee.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["firstname"]; ?><br>
+<?//put conditional statement for active/inactive status?>
+Your Person ID is: <?php echo $_POST["person_id"]; ?>
+
+
+
+
+</body>
+</html>
